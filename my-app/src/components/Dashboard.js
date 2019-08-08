eae4adbf99924aa80da84cad11f84e74883d9a42
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-08-08 15:29:05 -0500

    Added Display component to dashboard - passed props

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -5,8 +5,43 @@ const Dashboard = () => {
     const [ballCount, setBallCount] = useState(0);
     const [strikeCount, setStrikeCount] = useState(0);
     
+    const countBalls = () => {
+        if (ballCount < 4) {
+            setBallCount(ballCount + 1);
+        } else 
+        {
+            setBallCount(0);
+        }
+      };
+      const countStrikes = () => {
+        if (strikeCount < 3) {
+            setStrikeCount(strikeCount + 1);
+        } else 
+        {
+            setStrikeCount(0);
+        }
+      };
+    
+      const countFouls = () => {
+        if (strikeCount === 0) {
+            setStrikeCount(1);
+        } 
+        else if (strikeCount === 1) {
+            setStrikeCount(2);
+        } 
+        else {
+            setStrikeCount(strikeCount);
+        }
+      };
+
     return (
       <div className="dashboard">
+        <Display
+        balls={ballCount}
+        setBallCount={setBallCount}
+        strikes={strikeCount}
+        setStrikeCount={setStrikeCount}
+      />
       </div>
     );
   }
